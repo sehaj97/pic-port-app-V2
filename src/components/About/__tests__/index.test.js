771288ef28fe37ac6e6ce96de1ee04f0f381d69d
Sehajpreet Singh <sehajmagan@gmail.com>
2023-01-26 23:52:43 -0500

    practicing TDD

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,11 @@
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import About from "..";
+afterEach(cleanup);
+
+describe("About component", () => {
+  // First Test
+  it("renders", () => {
+    render(<About />);
+  });
+});
