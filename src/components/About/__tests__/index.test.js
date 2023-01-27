a275e5bcf0b1dcd523deee12e09ff4a64fcf5db5
Sehajpreet Singh <sehajmagan@gmail.com>
2023-01-27 01:20:03 -0500

    updated snapshot tests

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -8,4 +8,9 @@ describe("About component", () => {
   it("renders", () => {
     render(<About />);
   });
+
+  it("matches snapshot DOM node structure", () => {
+    const { asFragment } = render(<About />);
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
